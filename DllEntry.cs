a96f52da7cfd4bc54157d760f5f966e9d10534c9
k0s71k <109237243+k0s71k@users.noreply.github.com>
2023-09-20 00:47:39 +0300

    Update DllEntry.cs

--- DllEntry.cs
+++ DllEntry.cs
@@ -7,14 +7,9 @@ namespace MakeFile
 {
     public class DLLFuncs
     {
-        private static DLLFuncs _instance;
-
         [DllExport("RVExtension", CallingConvention = CallingConvention.StdCall)]
         public static void RVExtension(StringBuilder output, int outputSize, string function)
         {
-            if (_instance == null)
-                _instance = new DLLFuncs();
-
             int index = function.IndexOf('|');
             string name = function.Substring(0, index);
             string data = function.Substring(index + 1);
